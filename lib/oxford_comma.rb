b509a9600305fba0388a4de6a56cfd58a398f73d
Bry Cox <carl.cc@gmail.com>
2019-01-29 16:09:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ arr = []
 arr << array[-1]
 array.pop
 array.each {|x| x.insert(-1,",")}
+array[0].pop if array.count < 2
 array.insert(-1, "and") if array.count >= 1
 array.insert(-1, arr[0])
 array.join(" ")
