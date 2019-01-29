0fc6365454d288428152133cceb34ca04dae5670
Bry Cox <carl.cc@gmail.com>
2019-01-29 14:20:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
 array.insert(-2, "and") if array.count >= 2
-array.each {|x| x.insert(-1, ",") if x != "and" && array[-1]}
+array.each {|x| x.insert(-1, ",") if x != "and"}
 end
